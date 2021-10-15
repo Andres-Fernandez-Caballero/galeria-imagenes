71de9683e35256735246a58d7b78b028be3028b2
Andres <andres.fernandezcaballero@davinci.edu.ar>
2021-10-14 21:01:10 -0300

    Modifica carga de imagenes inicial, por defecto mostrara imagenes de gatitos

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 const Cards = () => {
 
     const [images, setImages] = useState([]);
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('cats')
 
     const peticion = useCallback(async () => {
 
